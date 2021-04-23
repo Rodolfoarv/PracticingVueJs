21aa640073dd7e45ffa5097e24a5e1154b528001
Rodolfo Andrés Ramírez Valenzuela <rodolfo@ramirezvalenzuela.com>
2021-04-23 17:08:02 +0300

    Integration of Backend / ADDED Fetching operation (#2)
    * ADDED Typescript types for backend structure
    
    * Fixed some issues with linting and prettier
    
    * Initial implementation of fetching the backend for GETALLTASKS operation
    
    * ADDED Display on UI the response from the backend

--- client/src/components/TodoItem.vue
+++ client/src/components/TodoItem.vue
@@ -1,15 +1,17 @@
 <template lang="pug">
-  .todo-item
-    <div>
-      <li>Test</li>
-    </div>
+  li.todo-item
+    p.todo-item_description {{ todo.description }}
+
 </template>
 
-<script>
-import { defineComponent } from '@vue/composition-api';
+<script lang="ts">
+import { defineComponent,PropType } from '@vue/composition-api';
+
+import { Todo } from '@/types/Todo';
 
 export default defineComponent({
   name: 'todo-item',
+  props: { todo: {type: Object as PropType<Todo>, required: true}},
   data() {
     return {};
   },
@@ -17,6 +19,21 @@ export default defineComponent({
 
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import './styles/mixins.scss';
+.todo-item {
+  display: flex;
+  justify-content: flex-start;
+  min-height: 5rem;
+  align-items: center;
+  text-align: center;
+  border-top: 1px solid #bbbbbbf8;
+  &_description {
+    text-align: left;
+    padding: 10px ;
+    word-break: break-all;
 
+  }
+
+}
 </style>
