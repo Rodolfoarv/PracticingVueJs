0fddb0dca5951629a6353c61a7e95e714dfb0e22
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2021-04-24 12:07:21 +0300

    ADDED Methods for updating, deleting and creating a new Todo task

--- client/src/services/api/todoApiBitPanda.ts
+++ client/src/services/api/todoApiBitPanda.ts
@@ -1,35 +1,72 @@
 import { Pagination } from '../../types/Pagination';
 import { Todo } from '../../types/Todo';
 
-export interface FetchTodosResponse {
+export interface GetAllTodosResponse {
   items: Todo[];
   meta: Pagination;
 }
 
-export interface FetchTodosOptions {
+export interface GetAllTodosOptions {
   offset: number;
   limit?: number;
   description?: string;
 }
 
-//Change this to .env later on
 const apiUrl = 'http://localhost:3000/api/v1';
 
 export function fetchTodos(
-  options: FetchTodosOptions
-): Promise<FetchTodosResponse> {
+  options: GetAllTodosOptions
+): Promise<GetAllTodosResponse> {
   const url = new URL(`${apiUrl}/todo`);
-  const params = new URLSearchParams({
+  const parameters = new URLSearchParams({
     offset: options.offset.toString(),
   });
 
-  if (options.limit) params.set('limit', options.limit.toString());
+  if (options.limit) parameters.set('limit', options.limit.toString());
 
-  if (options.description) params.set('description', options.description);
+  if (options.description) parameters.set('description', options.description);
 
-  url.search = params.toString();
-
+  url.search = parameters.toString();
+  
   return fetch(url.toString()).then((res) =>
     res.json()
-  ) as Promise<FetchTodosResponse>;
+  ) as Promise<GetAllTodosResponse>;
 }
+
+export async function createTodo(description: string): Promise<Todo> {
+  const url = new URL(`${apiUrl}/todo`);
+
+  return fetch(url.toString(), {
+    method: 'POST',
+    body: JSON.stringify({ description }),
+    headers: new Headers({ 'Content-type': 'application/json' }),
+  }).then((res) => res.json()) as Promise<Todo>;
+  
+}
+
+export async function deleteTodo(todo: Todo): Promise<boolean> {
+  const url = new URL(`${apiUrl}/todo/${todo._id}`);
+  let response;
+
+  try{
+    response = await fetch(url.toString(), {
+      method: 'DELETE',
+    });
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+  return response.ok;
+}
+
+
+export function updateTodo(todo: Todo): Promise<Todo> {
+  const url = new URL(`${apiUrl}/todo/${todo._id}`);
+
+  return fetch(url.toString(), {
+    method: 'PUT',
+    body: JSON.stringify(todo),
+    headers: new Headers({ 'Content-type': 'application/json' }),
+  }).then((res) => res.json()) as Promise<Todo>;
+}
+
